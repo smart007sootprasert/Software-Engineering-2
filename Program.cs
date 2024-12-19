c9c82dadc0450cc6d11a1fde0add4e043b053539
smart007sootprasert <smartsoodprasert69@gmail.com>
2024-12-19 21:10:18 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Programming4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your height in cm: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your full name is {firstName} {lastName}.");
+
+            Console.WriteLine($"\tYour height is {height} cm and you are {age} years old.");
+        }
+    }
+}
